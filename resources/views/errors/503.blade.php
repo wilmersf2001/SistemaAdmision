3ca4e1f5f99e2b595bec23c0b770f67f0b3be335
Wilmer Suclupe Farroñan <124706662+wilmersf2001@users.noreply.github.com>
2024-01-17 16:45:58 -0500

    se implemento la vista de mantenimiento

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<title>Sitio en Mantenimiento</title>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article>
+    <h1>
+        ¡Estaremos de vuelta pronto!</h1>
+    <div>
+        <p>Disculpe las molestias, pero estamos realizando tareas de mantenimiento en este momento. Si lo necesita,
+            siempre puede contactarnos; de lo contrario, <a href="mailto:#">contact us</a>, ¡volveremos a estar en línea
+            en breve!</p>
+        <p>&mdash; El equipo OTI</p>
+    </div>
+</article>
